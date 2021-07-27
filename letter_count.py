f33cbc7a44a13e3a5655f41812993c6afe3776dd
sreekanesh <86845817+sreekanesh@users.noreply.github.com>
2021-07-27 09:44:45 +0530

    Add files via upload

---
+++ letter_count.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+W= input('Please enter a string ')
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print (most_frequent(W))
